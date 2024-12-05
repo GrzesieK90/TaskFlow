7e29b39455dcb29e16f068fd88c11fd3316d5e87
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2024-12-05 12:40:56 +0100

    Solution for deploy errors

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  }
 })
